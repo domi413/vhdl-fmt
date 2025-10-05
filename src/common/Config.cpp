31d83b4a127ee070ef8a7f707e23662634fa777f
domi413 <dom.buehler@outlook.com>
2025-10-05 23:15:57 +0200

    use this

--- src/common/Config.cpp
+++ src/common/Config.cpp
@@ -6,22 +6,18 @@ namespace vhdl_fmt {
 
 auto Config::validate() const -> void
 {
-    if (line_config.line_length < MIN_LINE_LENGTH || line_config.line_length > MAX_LINE_LENGTH) {
-        const std::string msg = "Line length must be between "
-                              + std::to_string(MIN_LINE_LENGTH)
-                              + " and "
-                              + std::to_string(MAX_LINE_LENGTH)
-                              + ".";
+    if (this->line_config.line_length < MIN_LINE_LENGTH
+        || this->line_config.line_length > MAX_LINE_LENGTH) {
+        const std::string msg = "Line length must be between " + std::to_string(MIN_LINE_LENGTH)
+                              + " and " + std::to_string(MAX_LINE_LENGTH) + ".";
 
         throw std::invalid_argument(msg);
     }
 
-    if (line_config.indent_size < MIN_INDENT_SIZE || line_config.indent_size > MAX_INDENT_SIZE) {
-        const std::string msg = "Indent size must be between "
-                              + std::to_string(MIN_INDENT_SIZE)
-                              + " and "
-                              + std::to_string(MAX_INDENT_SIZE)
-                              + ".";
+    if (this->line_config.indent_size < MIN_INDENT_SIZE
+        || this->line_config.indent_size > MAX_INDENT_SIZE) {
+        const std::string msg = "Indent size must be between " + std::to_string(MIN_INDENT_SIZE)
+                              + " and " + std::to_string(MAX_INDENT_SIZE) + ".";
 
         throw std::invalid_argument(msg);
     }
