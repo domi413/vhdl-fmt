ec0e62f2447cddf5537e0874948d244db6f25eb0
domi413 <dom.buehler@outlook.com>
2025-10-08 00:23:32 +0200

    remove old main

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,10 +1,23 @@
+#include "ANTLRInputStream.h"
+#include "CommonTokenStream.h"
 #include "argument_parser.hpp"
+#include "ast/nodes/design_file.hpp"
+#include "builder/assembly/assembler.hpp"
+#include "builder/translator.hpp"
+#include "builder/visitor.hpp"
 #include "config_reader.hpp"
+#include "emit/debug_printer.hpp"
+#include "vhdlLexer.h"
+#include "vhdlParser.h"
 
+#include <cstddef>
 #include <cstdlib>
 #include <exception>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <span>
+#include <sstream>
 
 /// The main entry point of the program
 auto main(int argc, char *argv[]) -> int
@@ -18,6 +31,34 @@ auto main(int argc, char *argv[]) -> int
 
         // Call the formatter and pass the config object
         // formatter{ config_reader.readConfigFile() };
+
+        /* NOTE: The following code is from the previous main function
+
+           std::stringstream buffer;
+           buffer << in.rdbuf();
+
+           antlr4::ANTLRInputStream input(buffer.str());
+           vhdlLexer lexer(&input);
+           antlr4::CommonTokenStream tokens(&lexer);
+           tokens.fill();
+
+           vhdlParser parser(&tokens);
+           auto *tree = parser.design_file();
+
+           //--- AST construction pipeline ---
+           ast::DesignFile root;
+           builder::Assembler builder(root.units);
+
+           builder::Translator translator(builder, tokens);
+
+           builder::Visitor visitor(translator);
+           visitor.walk(tree);
+
+           std::cout << tree->toStringTree(&parser, true) << "\n\n";
+
+           emit::DebugPrinter printer;
+           root.accept(printer);
+        */
 
     } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << '\n';
